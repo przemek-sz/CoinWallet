cd9b102d7e6236374f30a4a8e650fb3bb050661b
Przemysław Szczerba <przemyslawszczerba@interia.pl>
2017-10-01 15:35:58 +0200

    Rout

--- src/main/java/com/szczerbap/coinwallet/config/WebConfig.java
+++ src/main/java/com/szczerbap/coinwallet/config/WebConfig.java
@@ -1,10 +1,13 @@
 package com.szczerbap.coinwallet.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 /**
  * Created by Przemek on 28.09.2017.
@@ -13,8 +16,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @ComponentScan({ "com.szczerbap.coinwallet" })
 @EnableWebMvc
 public class WebConfig extends WebMvcConfigurerAdapter {
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**").addResourceLocations("/static/");
+        registry.addResourceHandler("/partials/**").addResourceLocations("/static/partials/");
     }
+
+
 }
--- src/main/webapp/static/app/app.js
+++ src/main/webapp/static/app/app.js
@@ -2,7 +2,7 @@ var app = angular.module('app',['ngRoute']);
 
 app.config(function ($routeProvider) {
     $routeProvider.when('/loginpage',{
-        templateUrl:'partials/loginpage.html'
+        templateUrl:'static/partials/loginpage.html'
     });
 
 });
